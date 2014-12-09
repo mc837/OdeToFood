054eae625632b408c8344bf6126b5ca5aace5dc2
Matt Clarkson <Matt.clarkson@comparethemarket.com>
2014-12-09 11:50:44 +0000

    Work up to (and including)  Building Entities from Working with Data Part 1

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -3,9 +3,8 @@
     public class RestaurantReview
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
         public int Rating { get; set; }
+        public string Body { get; set; }
+        public int ResttaurantId { get; set; }
     }
 }
